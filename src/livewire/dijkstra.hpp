b2ed95b6e3478380ec0048a1004698f35c77b26b
TomekTTX <tomekttx@gmail.com>
2022-08-27 12:06:16 +0200

    Import Dijkstra optimizations

--- src/livewire/dijkstra.hpp
+++ src/livewire/dijkstra.hpp
@@ -1,8 +1,8 @@
 #ifndef DIJKSTRA_HPP
 #define DIJKSTRA_HPP
 
-#include <boost/heap/fibonacci_heap.hpp>
 #include <functional>
+#include <queue>
 #include <ranges>
 #include <type_traits>
 #include <vector>
@@ -12,55 +12,13 @@
 namespace detail {
 
 struct Node {
-    Point positon;
+    Point position;
     double priority;
 };
 
-struct CompareNodes {
-    bool operator()(const Node &l, const Node &r) const {
-        return l.priority > r.priority;
-    }
-};
-
-class Queue {
-    using Heap =
-        boost::heap::fibonacci_heap<Node, boost::heap::compare<CompareNodes>>;
-
-    std::vector<std::pair<Point, Heap::handle_type>> handles;
-    Heap heap;
-
-public:
-    Node extract() {
-        const auto top = heap.top();
-        handles.erase(std::remove_if(handles.begin(), handles.end(),
-                                     [&top](const auto &h) {
-                                         return h.first == top.positon;
-                                     }),
-                      handles.end());
-        heap.pop();
-        return top;
-    }
-
-    void add(const Point &p, double priority) {
-        const auto handle = heap.push({ p, priority });
-        handles.push_back({ p, handle });
-    }
-
-    void decrease(const Point &p, double priority) {
-        const auto handle =
-            std::find_if(handles.begin(), handles.end(),
-                         [&p](const auto &h) { return h.first == p; });
-        heap.decrease(handle->second, { p, priority });
-    }
-
-    bool empty() const { return heap.empty(); }
-
-    bool contains(const Point &p) const {
-        return std::find_if(handles.begin(), handles.end(),
-                            [&p](const auto &h) { return h.first == p; }) !=
-               handles.end();
-    }
-};
+constexpr bool operator<(const Node &l, const Node &r) {
+    return l.priority > r.priority;
+}
 
 template <class Enum>
 constexpr std::underlying_type_t<Enum> to_underlying(Enum e) noexcept {
@@ -83,25 +41,25 @@ std::vector<Point> dijkstra(const Graph &graph, const Point &source,
     Array2D<Point> prev{ graph.w(), graph.h(), { -1, -1 } };
     dist[source] = 0;
 
-    detail::Queue queue;
-    queue.add(source, dist[source]);
+    std::priority_queue<detail::Node> queue;
+    queue.push({ source, 0 });
 
     while (!queue.empty()) {
-        const auto u = queue.extract();
-        if (u.positon == dest) break;
-        for (const auto dir :
-             detail::enum_range(Dir::RIGHT, Dir::BOTTOMRIGHT)) {
-            const auto v = u.positon + dir;
-            if (v.x >= 0 && v.y >= 0 && v.x < graph.w() && v.y < graph.h()) {
-                const auto alt =
-                    dist[u.positon] + graph.distance(u.positon, dir);
-                if (alt < dist[v] && dist[u.positon] != INF) {
-                    dist[v] = alt;
-                    prev[v] = u.positon;
-                    if (queue.contains(v)) {
-                        queue.decrease(v, alt);
-                    } else {
-                        queue.add(v, alt);
+        const auto [position, priority] = queue.top();
+        queue.pop();
+        if (position == dest) break;
+        if (priority == dist[position]) {
+            for (const auto dir :
+                 detail::enum_range(Dir::RIGHT, Dir::BOTTOMRIGHT)) {
+                const auto v = position + dir;
+                if (v.x >= 0 && v.y >= 0 && v.x < graph.w() &&
+                    v.y < graph.h()) {
+                    const auto alt =
+                        dist[position] + graph.distance(position, dir);
+                    if (alt < dist[v] && dist[position] != INF) {
+                        dist[v] = alt;
+                        prev[v] = position;
+                        queue.push({ v, alt });
                     }
                 }
             }
